f19179aa808033bdb1d3914cfce443bbdc0045a0
Praveen Sharma <96952903+Praveen0079@users.noreply.github.com>
2026-09-10 15:41:24 +0530

    Modify SQL query for rolling total calculation

--- rolling_total.sql
+++ rolling_total.sql
@@ -1,5 +1,10 @@
-USE shop;
-SELECT *,SUM(amount) 
-OVER (ORDER BY payment_date
-	  ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as rolling_total
-FROM payments;
+use db;
+SELECT *,
+	SUM( CASE
+			WHEN order_status = 'Delivered' THEN quantity*price_per_unit 
+        ELSE 0
+        END)
+OVER(PARTITION BY category ORDER BY delivery_date
+ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)  as total
+from orders;
+
